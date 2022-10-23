d1e815a2f503ae1b081c2a7fc89a34a8cd141832
vcwai <victorcwai@gmail.com>
2022-10-23 10:32:24 +0800

    Prevent command line injection

--- stream.js
+++ stream.js
@@ -4,9 +4,11 @@ const stream = url => {
     function puts(error, stdout, stderr) {
         console.log(stdout);
     }
+    // should not have single quote, which will break the escaping
+    if (url.includes("'")) throw Error("Invalid url, please remove single quote.")
     // executes the command in the terminal
     // plays or streams the song
-    exec(`cvlc --no-video --play-and-exit ${url}`, puts);
+    exec(`cvlc --no-video --play-and-exit '${url}'`, puts);
 };
 
 module.exports = stream;
